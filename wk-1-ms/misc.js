1d110c1a9810180fe4b368151acabfeaf2cf2a9d
Anup Sharma <anupsharma555@gmail.com>
2026-08-29 13:08:55 -0400

    Modified misc.js with specific in-line suggestions

--- wk-1-ms/misc.js
+++ wk-1-ms/misc.js
@@ -1,18 +1,31 @@
 // Part 1 Exercises (use Copilot inline suggestions)
+// Function to calculate the factorial of a number
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
 
 // Exercise 1: Comment-Driven Development
 // Function to calculate the factorial of a number
 
 
 // Exercise 2: Array Operations
-const numbers = [1, 2, 3, 4, 5];
+const numList = [1, 2, 3, 4, 5];
 
 // Filter even numbers
+const evenNumbers = numList.filter(num => num % 2 === 0);
+
+// Map numbers to their squares
+const squaredNumbers = numList.map(num => num * num);
 
 
 // Exercise 3: Function from Signature
 // Type this on a new line and press Enter:
-// function reverseString(str)
+ function reverseString(str) {
+     return str.split('').reverse().join('');
+ }
 
 
-// Exercise 4: Process User Data 
+// Exercise 4: Process User Data
